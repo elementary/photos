364126da615a6803f772b5e401678d25a64d92e7
Cody Garver <cody@elementaryos.org>
2014-08-27 22:37:44 -0500

    Drop "Folders" sidebar feature

--- units.mk
+++ units.mk
@@ -25,8 +25,7 @@ UNITS = \
 	camera \
 	searches \
 	config \
-	data_imports \
-	folders
+	data_imports
 
 # Name(s) of units that represent application entry points.  These units will have init and
 # termination entry points generated: Name.unitize_init() and Name.unitize_terminate().  These
