elementary/photos

commit 86df00ced674abb2ee430ea24422079cfabb314c

Fix building against latest gexiv2 (#498)

src/photos/PhotoMetadata.vala
@@ -174,7 +174,11 @@ public class PhotoMetadata : MediaMetadata {
         exiv2 = new GExiv2.Metadata ();
         exif = null;
 
+#if GEXIV2_0_11
+        exiv2.open_buf (buffer[0:length]);
+#else
         exiv2.open_buf (buffer, length);
+#endif
         exif = Exif.Data.new_from_data (buffer, length);
         source_name = "<memory buffer %d bytes>".printf (length);
     }
@@ -188,8 +192,11 @@ public class PhotoMetadata : MediaMetadata {
         exiv2 = new GExiv2.Metadata ();
         exif = null;
 
-        exiv2.from_app1_segment (buffer, length);
+#if GEXIV2_0_11
+        exiv2.from_app1_segment (buffer[0:length]);
+#else
         exif = Exif.Data.new_from_data (buffer, length);
+#endif
         source_name = "<app1 segment %d bytes>".printf (length);
     }
 

meson.build
@@ -26,6 +26,9 @@ glib_dep = dependency('glib-2.0', version: '>=2.30.0')
 gio_unix_dep = dependency('gio-unix-2.0', version: '>=2.20')
 gee_dep = dependency('gee-0.8', version: '>=0.8.5')
 gexiv2_dep = dependency('gexiv2', version: '>=0.4.90')
+if gexiv2_dep.version().version_compare('>= 0.11')
+  add_global_arguments(['--define=GEXIV2_0_11'], language : 'vala')
+endif
 geocode_glib_dep = dependency('geocode-glib-1.0')
 gmodule_dep = dependency('gmodule-2.0', version: '>=2.24.0')
 gstreamer_dep = dependency('gstreamer-1.0', version: '>=1.0.0')

commit 88ee69b57f94efa2779595d1544109fed6a3211c

photos/PhotoMetadata: fix issue introduced with PR #498

src/photos/PhotoMetadata.vala
@@ -197,6 +197,7 @@ public class PhotoMetadata : MediaMetadata {
 #else
         exiv2.from_app1_segment (buffer, length);
 #endif
+        exif = Exif.Data.new_from_data (buffer, length);
         source_name = "<app1 segment %d bytes>".printf (length);
     }
 